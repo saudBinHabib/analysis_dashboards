8df5da22c0b33d8faeb1ca874884e9fd0c142f69
saudBinHabib <develop.android.apps01@gmail.com>
2024-12-16 00:23:35 +0100

    updating the fast_api application, to give match, and team level data.

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date
 
 # Add project root to Python path
 import sys
@@ -13,6 +14,7 @@ from typing import Dict, List, Tuple
 
 from common.data_loader import process_and_store_data
 from common.data_provider import (
+    get_matches_from_events,
     get_all_aerials_duels_by_players_count,
     get_all_passes_by_players_count,
     get_key_passes_by_players_count,
@@ -22,11 +24,9 @@ from common.data_provider import (
     get_successfull_aerials_duels_by_players_count,
     get_successfull_passes_by_players_count,
 )
-from common.database import session
+from common.database import session, logger
 
 app = FastAPI()
-
-team_id = "apoawtpvac4zqlancmvw4nk4o"
 
 
 @app.get("/")
@@ -44,8 +44,22 @@ async def process_data():
     return {"message": "Data processed and stored in the database!"}
 
 
+@app.get("/all_matches")
+async def get_all_matches() -> List[Dict]:
+    """
+    Get all matches data
+
+    :return: List[Dict[str, str | int | date]]
+
+    """
+    matches = get_matches_from_events(session, logger)
+    logger.info("Matches data retrieved successfully")
+    logger.info(matches)
+    return matches
+
+
 @app.get("/metrics")
-async def get_processed_data():
+async def get_metrics(team_id: str, match_id: str) -> Dict[str, List[int]]:
     """
     Get all metrics data
 
@@ -58,70 +72,116 @@ async def get_processed_data():
     "Number of Aerial Duels",
     "Aerial Duel Success",
 
+    :param team_id: str
+    :param match_id: str
+    :return: Dict[str, List[int]]
+
+
     """
-    return get_player_metrics(session, team_id)
+    return get_player_metrics(session, team_id, match_id)
 
 
 @app.get("/all_passes")
-async def get_all_passes():
+async def get_all_passes(team_id: str, match_id: str) -> List[Dict[str, int]]:
     """
     Get all passes data
+
+    :param team_id: str
+    :param match_id: str
+    :return: Dict[str, List[int]]
+
     """
-    all_passes = get_all_passes_by_players_count(session, team_id)
+    all_passes = get_all_passes_by_players_count(session, team_id, match_id)
     return [{pas[0]: pas[1]} for pas in all_passes]
 
 
 @app.get("/successfull_passes")
-async def get_successfull_passes():
+async def get_successfull_passes(team_id: str, match_id: str) -> List[Dict[str, int]]:
     """
     Get successfull passes data
+
+    :param team_id: str
+    :param match_id: str
+    :return: Dict[str, List[int]]
+
     """
-    successfull_passes = get_successfull_passes_by_players_count(session, team_id)
+    successfull_passes = get_successfull_passes_by_players_count(
+        session, team_id, match_id
+    )
     return [{pas[0]: pas[1]} for pas in successfull_passes]
 
 
 @app.get("/shots_on_goals")
-async def get_shots_on_goals():
+async def get_shots_on_goals(team_id: str, match_id: str) -> List[Dict[str, int]]:
     """
     Get shots on goals data
+
+    :param team_id: str
+    :param match_id: str
+    :return: Dict[str, List[int]]
+
     """
-    shots_on_goals = get_shots_on_goals_by_players_count(session, team_id)
+    shots_on_goals = get_shots_on_goals_by_players_count(session, team_id, match_id)
     return [{pas[0]: pas[1]} for pas in shots_on_goals]
 
 
 @app.get("/key_passes")
-async def get_key_passes():
+async def get_key_passes(team_id: str, match_id: str) -> List[Dict[str, int]]:
     """
     Get key passes data
+
+    :param team_id: str
+    :param match_id: str
+    :return: Dict[str, List[int]]
+
     """
-    key_passes = get_key_passes_by_players_count(session, team_id)
+    key_passes = get_key_passes_by_players_count(session, team_id, match_id)
     return [{pas[0]: pas[1]} for pas in key_passes]
 
 
 @app.get("/successfull_aerials_duels")
-async def get_successfull_aerials_duels():
+async def get_successfull_aerials_duels(
+    team_id: str, match_id: str
+) -> List[Dict[str, int]]:
     """
     Get successfull aerials duels data
+
+    :param team_id: str
+    :param match_id: str
+    :return: Dict[str, List[int]]
+
     """
     successfull_aerials_duels = get_successfull_aerials_duels_by_players_count(
-        session, team_id
+        session, team_id, match_id
     )
     return [{pas[0]: pas[1]} for pas in successfull_aerials_duels]
 
 
 @app.get("/all_aerials_duels")
-async def get_all_aerials_duels():
+async def get_all_aerials_duels(team_id: str, match_id: str) -> List[Dict[str, int]]:
     """
     Get all aerials duels data
+
+    :param team_id: str
+    :param match_id: str
+    :return: Dict[str, List[int]]
+
     """
-    all_aerials_duels = get_all_aerials_duels_by_players_count(session, team_id)
+    all_aerials_duels = get_all_aerials_duels_by_players_count(
+        session, team_id, match_id
+    )
     return [{pas[0]: pas[1]} for pas in all_aerials_duels]
 
 
 @app.get("/long_passes")
-async def get_long_passes():
+async def get_long_passes(team_id: str, match_id: str) -> List[Dict[str, int]]:
     """
     Get long passes data
+
+    :param team_id: str
+    :param match_id: str
+    :return: Dict[str, List[int]]
+
     """
-    long_passes = get_long_passes_by_players_count(session, team_id)
+    long_passes = get_long_passes_by_players_count(session, team_id, match_id)
     return [{pas[0]: pas[1]} for pas in long_passes]
